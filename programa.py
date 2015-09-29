505f0fcf40b126cbfbf0db733dc75bab328d49e1
tecnologiaslibres <info@tecnologiaslibres.coop>
2015-09-29 19:49:01 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,36 +1,40 @@
-#Ley de ohm (el programa aun esta en desarrollo)
+#Ley de ohm
+from __builtin__ import int
+global valor
+global voltaje
+global amperaje
+global resistencia
 
 print "Para calcular voltaje presione 1"
 print "Para calcular amperaje presione 2"
 print "Para calcular resistencia presione 3"
 valor = int(input("Ingrese la opcion: "))
-def comprobarDatos():
-	if (valor>0) or (valor<=3):
-		print ("Usted tecleo " + str(valor) + " Por favor, elija las opciones correctamente")
-		return
-comprobarDatos()
-# Calcular voltaje
+#Calcular voltaje
 if valor == 1:
-	amperaje = float(input("Ingrese amperes: "))
-	resistencia = float(input("Ingrese resistencia: "))
+    print ("Ud. calculara el voltaje")
+    print ("========================")
+    amperaje = float(input("Ingrese amperes: "))
+    resistencia = float(input("Ingrese resistencia: "))
 #Formula
-	voltaje = resistencia*amperaje
-	print (str(voltaje)+" volts")
-
+    voltaje = resistencia * amperaje
+    print (str(voltaje)+" volts")
 # Calcular amperaje
-
 if valor == 2:
-	voltaje = float(input("Ingrese voltaje: "))
-	resistencia = float(input("Ingrese resistencia: "))
-#Formula
-	amperaje = voltaje / resistencia
-	print (str(amperaje) + " ampers")
-
+    print ("Ud calculara el amperaje")
+    print ("========================")
+    resistencia = float(input("Ingrese resistencia: "))
+#formula
+    amperaje = voltaje / resistencia
+    print (str(amperaje) + " ampers")
 # Calcular resistencia
 
 if valor == 3:
-	voltaje = float(raw_input("Ingrese voltaje: "))
-	amperaje = float(raw_input("Ingrese amperaje: "))
-	resistencia = voltaje / amperaje
-#Formula
-	print (str(resistencia) + " omhs")
+    print ("Ud. calculara la resistencia")
+    print ("============================")
+    voltaje = float(raw_input("Ingrese voltaje: "))     
+    amperaje = float(raw_input("Ingrese amperaje: "))
+#formula
+    resistencia = voltaje / amperaje
+    print (str(resistencia) + " omhs")
+else:
+    print ("Ud ingreso " + str(valor) + " Por favor ingrese la opcion correcta")
